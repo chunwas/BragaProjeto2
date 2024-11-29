51c754fae940912089fc83a1aa881135ebe3f2d3
lucas1matheus <161655700+lucas1matheus@users.noreply.github.com>
2024-11-29 14:03:56 -0300

    Update Controle.java

--- Controle.java
+++ Controle.java
@@ -1,1 +1,40 @@
+public class Controle{
+  private Banco banco = null;
+  private Console console = null;
+  public Controle(){
+    console = console();
+    banco = banco();
+  }
+  public executar(){
+    int resposta = null;
 
+
+    while(resposta != 0){
+      resposta = console.menu();
+      switch(resposta){
+        case 1:
+          adicionarParcela();
+          break;
+        case 2:
+          regarParcela();
+          break;
+        case 3:
+          adubarParcela();
+          break;
+        case 4:
+          marcarTratamentoEspecial();
+          break;
+        case 5:
+          sugerirAcoes();
+          break;
+        case 6:
+          gerarRelatorio();
+          break;
+        case 0:
+          console.MensagemDeFinalizacao();
+          break;
+        default:
+          break;
+  
+  
+}
